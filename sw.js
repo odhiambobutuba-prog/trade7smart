586a771c2cf18c95080c4da859ddb16a1802dc50
Trade7smart <odhiambobutuba@gmail.com>
2026-06-27 18:13:14 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,45 +1,4 @@
-const CACHE = "trade7smart-v5-fresh-20260621";
-const FILES = [
-  "./",
-  "./index.html",
-  "./styles.css?v=v5-fresh-20260621",
-  "./app.js?v=v5-fresh-20260621",
-  "./manifest.webmanifest?v=v5-fresh-20260621",
-  "./icon.svg"
-];
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
-  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(FILES)));
-});
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys()
-      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))
-      .then(() => self.clients.claim())
-  );
-});
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-  event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        const copy = response.clone();
-        caches.open(CACHE).then((cache) => cache.put(event.request, copy));
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
-});
-self.addEventListener("notificationclick", (event) => {
-  event.notification.close();
-  const targetUrl = event.notification.data?.url || "./";
-  event.waitUntil(
-    clients.matchAll({ type: "window", includeUncontrolled: true }).then((clientList) => {
-      for (const client of clientList) {
-        if ("focus" in client) return client.focus();
-      }
-      if (clients.openWindow) return clients.openWindow(targetUrl);
-      return undefined;
-    })
-  );
-});
+const C='t7s-v1';
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(clients.claim()));
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
